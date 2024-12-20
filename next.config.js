f31d195d60e509aafdd1151b0a8aaa1b4be6b1bb
Pravin <boppuripravin@gmail.com>
2024-12-20 15:46:02 +0100

    config for nextjs

---
+++ next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    rewrites: async () => {
+      return [
+        {
+          source: "/backend/py/:path*",
+          destination:
+            process.env.NODE_ENV === "production"
+              ? "http://127.0.0.1:8000/backend/py/:path*"
+              : "/api/",
+        },
+        {
+          source: "/docs",
+          destination:
+            process.env.NODE_ENV === "production"
+              ? "http://127.0.0.1:8000/backend/py/docs"
+              : "/backend/py/docs",
+        },
+        {
+          source: "/openapi.json",
+          destination:
+            process.env.NODE_ENV === "production"
+              ? "http://127.0.0.1:8000/backend/py/openapi.json"
+              : "/backend/py/openapi.json",
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
